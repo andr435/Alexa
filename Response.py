0e52d4f202d0327aeca56ee3b1daeb0078299c30
andr435 <andr435@gmail.com>
2017-12-13 10:43:24 +0000

    start response class

--- Response.py
+++ Response.py
@@ -1,1 +1,26 @@
 class Response:
+    api_version = "1.0"
+    speach = {}
+    card = None
+    repromt = None
+    directives = None
+
+    def __init__(self, text, type='text'):
+        if type == 'text':
+            self.speach = {"type": "PlainText", "text": text}
+        else:
+            self.speach = {"type": "SSML", "ssml": text}
+
+    def set_text(self, text):
+        self.speach = {"type": "PlainText", "text": text}
+
+    def set_ssml(self, text):
+        self.speach = {"type": "SSML", "ssml": text}
+
+    def set_card(self, title, text, small_pic=None, big_pic=None):
+        skip
+
+    def set_card_simple(selftitle, text):
+        skip
+    
+        
